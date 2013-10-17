e103c9c069c7692ae2c7920253ebd70048d81601
Shayan Mohanty <shayan@evolucix.com>
2013-10-16 19:57:11 -0500

    Changed paths. Getting ready for Mocha + Chai add-ins

---
+++ README.md
@@ -0,0 +1,8 @@
+TODO
+========
+
++ Install Mocha + Chai for API testing
+
++ Add Mocha + Chai to GruntFile
+
++ Optimize GruntFile for better workflow
--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,8 @@ files = [
   'app/bower_components/angular-mocks/angular-mocks.js',
   'app/scripts/*.js',
   'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
+  'test/front/mock/**/*.js',
+  'test/front/spec/**/*.js'
 ];
 
 // list of files to exclude
